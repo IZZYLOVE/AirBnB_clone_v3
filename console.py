887618ecc4895df9ef99fffa6a7e238f5d5d94f8
DevMaxwell <89994817+DevMaxwell@users.noreply.github.com>
2023-07-31 21:07:22 +0000

    console.py

--- console.py
+++ console.py
@@ -5,10 +5,10 @@ import cmd
 from datetime import datetime
 import models
 from models.amenity import Amenity
-from models.base_model import BaseModel
 from models.city import City
 from models.place import Place
 from models.review import Review
+from models.base_model import BaseModel
 from models.state import State
 from models.user import User
 import shlex  # for splitting the line along spaces except in double quotes
